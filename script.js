d269e80479b10b415004bc4ec2d7c1dc6c362311
Anupama Aravind <asha.aravind1995@gmail.com>
2023-04-06 14:57:42 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log('hello');
+let x = 3;
+console.log(x);
+
+ // looping
+ 
+ //for loop
+ for(let a =0;a<=10;a++){
+    console.log(a);
+ }
+
+ //while loop
+
+ let b =0;
+while(b<=10){
+    console.log(b);
+    b++;
+}
+
+// do while
+let i=2;
+do{
+    console.log(i);
+}while(i>2)
+
+//for...of loop
+
+//dbt??
